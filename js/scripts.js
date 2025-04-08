e678c97bf39b868f6ab5d19a1948c021f6c69f79
Fernando Muñoz <fernando1986caballeria@gmail.com>
2025-04-08 14:52:20 -0500

    Cambio de fondo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
 // archivo para los scripts 
+
+function colorAleatorio() {
+    const letras = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letras[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function cambiarFondoTitulo() {
+    const titulo = document.querySelector("h1");
+    const color1 = colorAleatorio();
+    const color2 = colorAleatorio();
+    const color3 = colorAleatorio();
+
+    titulo.style.background = `linear-gradient(to right, ${color1}, ${color2}, ${color3})`;
+    titulo.style.color = "#fff"; 
+    titulo.style.padding = "10px"; 
+    titulo.style.borderRadius = "8px";
+}
+
+const navbar = document.getElementById("navbar");
+
+navbar.addEventListener("click", function (e) {
+    e.preventDefault(); 
+
+    cambiarFondoTitulo();
+});
